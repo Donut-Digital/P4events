f17ac9901f4b68c9ec61137b61b8447d054ee637
Eduards Gzibovskis <gzibovskis1998@gmail.com>
2023-04-20 23:07:02 +0100

    Added brd block component.

---
+++ resources/views/components/bard_block.blade.php
@@ -0,0 +1,6 @@
+<section class="flex flex-col lg:flex-row container xl:max-w-screen-xl m-auto py-5">
+    <div class="bard">
+        <hr class="h-1 w-24 my-4 border-none" style="background-color: {{ $component['bard_highlight'] }}">
+        {!! $component['bard'] !!}
+    </div>
+</section>
