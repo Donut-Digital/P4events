7031048df306b94fcf4c545bc1a32c3b6a90e9fc
nlloydw <nlloydw61@gmail.co.uk>
2023-04-21 11:36:11 +0100

    hotfix - added a color theme option to the intro block to prevent the text from being hidden with a bright bh

--- resources/views/components/intro_block.blade.php
+++ resources/views/components/intro_block.blade.php
@@ -1,13 +1,16 @@
-<section class="relative mb-10" style="background-color: {{ $component['background_color'] }};">
+<section @class(["relative mb-10","border-b border-davy-gray border-opacity-25" =>$component['light_theme']]) style="background-color: {{ $component['background_color'] }};">
 
     <div class="flex flex-col text-center">
         <div class="bg-cover bg-center absolute h-full opacity-10 w-full" style="background-image: url({{ $component['background_image'] }});"></div>
         <div @class(['z-10 py-12 max-w-3xl m-auto','py-24' => $component['button_label'] && $component['button']])>
 
-            <h1 class="text-white">{{ $component['title'] }}</h1>
+            <h1 @class(["text-white"=>!$component['light_theme'],
+                        "text-davy-gray"=>$component['light_theme']])>{{ $component['title'] }}</h1>
 
             @if($component['introduction'])
-                <p class="pt-4 text-white font-light">{{ $component['introduction'] }}</p>
+                <p @class(["pt-4 font-light",
+                           "text-white"=>!$component['light_theme'],
+                           "text-davy-gray"=>$component['light_theme']])>{{ $component['introduction'] }}</p>
             @endif
 
             @if($component['button_label'] && $component['button'])
