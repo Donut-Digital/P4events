396629007aec2d5a4ef8bfc0cfb32b2ed08369a8
nlloydw <nlloydw61@gmail.co.uk>
2023-04-22 20:07:30 +0100

    added the product category block

--- resources/js/components/component.vue
+++ resources/js/components/component.vue
@@ -1,12 +1,12 @@
+<script>
+export default {
+  name: "home"
+}
+</script>
+
 <template>
     <h1 class="text-3xl font-bold underline text-red-400">Home</h1>
 </template>
-
-<script>
-export default {
-    name: "home"
-}
-</script>
 
 <style scoped>
 
