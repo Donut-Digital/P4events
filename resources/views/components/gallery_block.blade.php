1cb631d0514ca7309a99a0cf0fc777200d8c99fd
nlloydw <nlloydw61@gmail.co.uk>
2023-06-11 14:06:45 +0100

    updated the gallery blocks to include a CTA

--- resources/views/components/gallery_block.blade.php
+++ resources/views/components/gallery_block.blade.php
@@ -10,10 +10,10 @@
         @if (count($component['gallery']) <= 2)
 
             <div class="flex flex-row gap-4">
-                <img src="{{ $component['gallery'][0]['permalink'] }}" alt="{{ $component['gallery'][0]['alt'] }}" class="min-w-0 flex-1 object-cover" loading="lazy">
+                <img src="{{ $component['gallery'][0]['image']['permalink'] }}" alt="{{ $component['gallery'][0]['image']['alt'] }}" class="min-w-0 flex-1 object-cover" loading="lazy">
 
                 @if(isset($component['gallery'][1]))
-                    <img src="{{ $component['gallery'][1]['permalink'] }}" alt="{{ $component['gallery'][1]['alt'] }}" class="min-w-0 flex-1 object-cover" loading="lazy">
+                    <img src="{{ $component['gallery'][1]['permalink'] }}" alt="{{ $component['gallery'][1]['image']['alt'] }}" class="min-w-0 flex-1 object-cover" loading="lazy">
                 @endif
             </div>
 
@@ -25,13 +25,44 @@
             <!-- $component['image_position'] == 'Left' -->
 
             <div class="flex gap-4">
-                <div @class(["w-1/3 flex flex-col justify-between gap-4","order-last"=>$component['main_image_position']->value() === 'Left'])>
-                    <img src="{{ $firstImage['permalink'] }}" alt="{{ $firstImage['alt'] }}" class="w-full h-1/2 object-cover" loading="lazy">
-                    <img src="{{ $secondImage['permalink'] }}" alt="{{ $secondImage['alt'] }}" class="w-full h-1/2 object-cover" loading="lazy">
+
+                <div @class(["w-1/3 flex flex-col justify-between gap-4","order-last"=>$component['main_image_position']->value() === 'left'])>
+
+                    <div class='relative inline-block hero-block w-full h-full'>
+                        <img class="w-full h-full object-cover" src="{{ $firstImage['image']['permalink'] }}" alt="{{ $firstImage['image']['alt'] }}" loading="lazy">
+
+                        @if($firstImage['add_cta'] && $firstImage['entry'])
+                            <a href="{{ $firstImage['entry']['permalink'] }}" class='cursor-pointer w-full h-full flex flex-col opacity-0 hover:opacity-100 transition-all flex items-center justify-center md:absolute bottom-0' style="background-color: #e22631d4">
+                                <h3 class="text-white font-medium">{{ $firstImage['title'] }}</h3>
+                                <p class="text-white"> {{$firstImage['description']}}</p>
+                            </a>
+                        @endif
+                    </div>
+
+                    <div class="relative inline-block hero-block w-full h-full">
+                        <img class="w-full h-full object-cover" src="{{ $secondImage['image']['permalink'] }}" alt="{{ $secondImage['image']['alt'] }}" loading="lazy">
+
+                        @if($secondImage['add_cta'] && $secondImage['entry'])
+                            <a href="{{ $secondImage['entry']['permalink'] }}" class='cursor-pointer w-full h-full flex flex-col opacity-0 hover:opacity-100 transition-all flex items-center justify-center md:absolute bottom-0' style="background-color: #e22631d4">
+                                <h3 class="text-white font-medium">{{ $secondImage['title'] }}</h3>
+                                <p class="text-white"> {{$secondImage['description']}}</p>
+                            </a>
+                        @endif
+                    </div>
+
                 </div>
-                <div class="w-2/3">
-                    <img class="w-full h-full object-cover" src="{{ $thirdImage['permalink'] }}" alt="{{ $thirdImage['alt'] }}" loading="lazy">
+
+                <div class='w-2/3 relative inline-block hero-block w-full'>
+                    <img class="w-full h-full object-cover" src="{{ $thirdImage['image']['permalink'] }}" alt="{{ $thirdImage['image']['alt'] }}" loading="lazy">
+
+                    @if($thirdImage['add_cta'] && $thirdImage['entry'])
+                        <a href="{{ $thirdImage['entry']['permalink'] }}" class='cursor-pointer w-full h-full flex flex-col opacity-0 hover:opacity-100 transition-all flex items-center justify-center md:absolute bottom-0' style="background-color: #e22631d4">
+                            <h3 class="text-white font-medium">{{ $thirdImage['title'] }}</h3>
+                            <p class="text-white"> {{$thirdImage['description']}}</p>
+                        </a>
+                    @endif
                 </div>
+
             </div>
 
         @endif
@@ -40,7 +71,7 @@
 
         <div class="flex flex-col lg:flex-row gap-5">
             @foreach($component['gallery'] as $image)
-                <img class="object-cover w-full flex-1 lg:w-1/3 h-[250px] min-w-0" src="{{ $image['permalink'] }}" alt="{{ $image['alt'] }}" loading="lazy">
+                <img class="object-cover w-full flex-1 lg:w-1/3 h-[250px] min-w-0" src="{{ $image['image']['permalink'] }}" alt="{{ $image['image']['alt'] }}" loading="lazy">
             @endforeach
         </div>
 
