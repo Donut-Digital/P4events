364e2f9e0727ad2dfcb287b5ca2eadcb9f647205
nlloydw <nlloydw61@gmail.co.uk>
2023-06-13 14:11:06 +0100

    made the hero block more responsive with unlimited sections

--- resources/views/components/hero_block.blade.php
+++ resources/views/components/hero_block.blade.php
@@ -2,16 +2,16 @@
 
         @foreach($component['hero_sections'] as $hero_block)
             <div @class(['relative inline-block hero-block',
-                         'w-full' => $loop->index == 0,
-                         'w-full lg:flex-1'=>$loop->index !== 0])>
+                         'w-full'=>$loop->index == 0 || $loop->index % 3 == 0,
+                         'basis-full lg:basis-1/3 lg:flex-1 max-h-[450px]'=>$loop->index !== 0 && $loop->index % 3 !== 0])>
 
-                <img class="w-full h-full" src="{{ $hero_block['hero_image'] }}" alt="{{ $hero_block['hero_image']['alt'] }}" loading="lazy">
+                <img class="w-full h-full object-cover" src="{{ $hero_block['hero_image'] }}" alt="{{ $hero_block['hero_image']['alt'] }}" loading="lazy">
 
                 @if($hero_block['bard_color'] && $hero_block['bard_field'])
 
                     <div @class(['w-full lg:w-4/5 md:absolute bottom-0 p-6 lg:p-8 lg:mb-20',
-                                 'lg:text-right lg:right-0' => $loop->index == 0,
-                                 'text-left lg:left-0'=>$loop->index !== 0,
+                                 'lg:text-right lg:right-0' => $loop->index == 0 || $loop->index % 3 == 0,
+                                 'text-left lg:left-0'=>$loop->index !== 0 && $loop->index % 3 !== 0,
                                  "bard-light"=>$hero_block['light_text'],
                                  "bard-dark"=>!$hero_block['light_text']])
                          style="background-color: {{ $hero_block['bard_color'] }}e8;">
