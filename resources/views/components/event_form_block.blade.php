d5c4a03454fa66116cdce85d15378c15c5381421
nlloydw <nlloydw61@gmail.co.uk>
2023-06-13 19:40:56 +0100

    responsive fixes

--- resources/views/components/event_form_block.blade.php
+++ resources/views/components/event_form_block.blade.php
@@ -1,17 +1,15 @@
 <section class="constraint mx-auto py-10 px-4">
-    <div class="flex flex-col items-center justify-center gap-4 text-center lg:flex-row lg:items-start lg:text-left">
+    <div class="flex flex-col lg:flex-row gap-4">
         {{-- Image --}}
-        <div class="w-full lg:w-1/3">
-            <div class="flex h-full flex-col">
-                <img class="hidden lg:block" src="{{ $component['image'] }}" alt="{{ $component['alt'] }}" loading="lazy">
-                <div class="p-5 bg-primary event-form-bard">
-                    {!! $component['bard'] !!}
-                </div>
+        <div class="flex w-full lg:w-1/3 flex-col">
+            <img class="hidden lg:block basis-full flex-1" src="{{ $component['image'] }}" alt="{{ $component['alt'] }}" loading="lazy">
+            <div class="p-5 bg-primary event-form-bard flex-1">
+                {!! $component['bard'] !!}
             </div>
         </div>
 
         {{-- Form details --}}
-        <div class="flex w-full lg:w-2/3 flex-col gap-8 lg:sticky top-0">
+        <div class="flex w-full lg:w-2/3 flex-col gap-8">
             <event-form csrf="{{ csrf_token() }}" origin="{{ $component['type'] }}">
             </event-form>
         </div>
--- resources/views/components/join_us_form_block.blade.php
+++ resources/views/components/join_us_form_block.blade.php
@@ -1,21 +1,18 @@
-<section class="constraint mx-auto py-10 px-4">
-    <div class="flex flex-col items-center justify-center gap-4 text-center lg:flex-row lg:items-start lg:text-left">
-        {{-- Image --}}
-        <div class="w-full lg:w-1/3">
-            <div class="flex h-full flex-col">
-                <img class="hidden lg:block" src="{{ $component['main_image'] }}" alt="{{ $component['main_image']['alt'] }}" loading="lazy">
-                <div class="p-5 bg-primary">
-                    <p class="font-bold text-white">{{ $component['image_title'] }}</p>
-                    <p class="text-white">{{ $component['image_sub_description'] }}</p>
-                </div>
-            </div>
-        </div>
+<section class="mx-auto px-4 py-10 constraint flex flex-col lg:flex-row gap-4">
 
-        {{-- Form details --}}
-        <div id="join_us" class="flex w-full lg:w-2/3 flex-col gap-8 lg:sticky top-0">
-            <join-us-form csrf="{{ csrf_token() }}" origin="{{ $component['type'] }}"
-                      tcs="{{ $component['terms_conditions']['permalink'] }}">
-            </join-us-form>
+    <div class="flex w-full lg:w-1/3 flex-col">
+        <img class="hidden lg:block basis-full flex-1" src="{{ $component['main_image'] }}" alt="{{ $component['main_image']['alt'] }}" loading="lazy">
+        <div class="p-5 bg-primary flex-1">
+            <p class="font-bold text-white">{{ $component['image_title'] }}</p>
+            <p class="text-white">{{ $component['image_sub_description'] }}</p>
         </div>
     </div>
+
+    {{-- Form details --}}
+    <div id="join_us" class="flex w-full lg:w-2/3 flex-col gap-8">
+        <join-us-form csrf="{{ csrf_token() }}" origin="{{ $component['type'] }}"
+                  tcs="{{ $component['terms_conditions']['permalink'] }}">
+        </join-us-form>
+    </div>
+
 </section>
