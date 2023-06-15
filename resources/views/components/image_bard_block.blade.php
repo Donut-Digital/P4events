92c785799691fdea5250852422025609fab50a76
nlloydw <nlloydw61@gmail.co.uk>
2023-06-15 19:41:55 +0100

    image fit fix

--- resources/views/components/image_bard_block.blade.php
+++ resources/views/components/image_bard_block.blade.php
@@ -1,6 +1,6 @@
 <section class="container m-auto flex flex-col gap-4 p-4 lg:flex-row xl:max-w-screen-xl">
 
-    <img src="{{ $component['image']['permalink'] }}" alt="{{ $component['image']['alt'] }}" class="w-full lg:w-1/2 min-w-0 max-h-[350px] object-cover" loading="lazy">
+    <img src="{{ $component['image']['permalink'] }}" alt="{{ $component['image']['alt'] }}" class="w-full lg:w-1/2 min-w-0 object-cover" loading="lazy">
     <div class="bard lg:w-1/2 flex flex-grow flex-col">
         <hr class="mb-4 h-1 w-24 border-none" style="background-color: {{ $component['bard_highlight'] }}">
         {!! $component['bard'] !!}
