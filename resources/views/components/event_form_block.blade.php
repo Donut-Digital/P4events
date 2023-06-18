e40d5840ac10cacee9e073775d0a34877e4b4d5e
nlloydw <nlloydw61@gmail.co.uk>
2023-06-18 20:41:53 +0100

    made event form block more responsivew

--- resources/views/components/event_form_block.blade.php
+++ resources/views/components/event_form_block.blade.php
@@ -1,15 +1,28 @@
 <section class="constraint mx-auto py-10 px-4">
     <div class="flex flex-col lg:flex-row gap-4">
         {{-- Image --}}
+
+        @if($component['image'])
         <div class="flex w-full lg:w-1/3 flex-col">
             <img class="hidden lg:block basis-full flex-1" src="{{ $component['image'] }}" alt="{{ $component['alt'] }}" loading="lazy">
             <div class="p-5 bg-primary event-form-bard flex-1">
                 {!! $component['bard'] !!}
             </div>
         </div>
+        @endif
 
         {{-- Form details --}}
-        <div class="flex w-full lg:w-2/3 flex-col gap-8">
+        <div @class([$component['image']=>'flex w-full lg:w-2/3 flex-col gap-8',
+                     !$component['image']=>'w-full'])>
+
+            @if(!$component['image'])
+                <hr class="my-4 h-1 w-24 border-none" style="background-color: #5d5d5d;">
+
+                <div class="py-5 bard">
+                    {!! $component['bard'] !!}
+                </div>
+            @endif
+
             <event-form csrf="{{ csrf_token() }}" origin="{{ $component['type'] }}">
             </event-form>
         </div>
