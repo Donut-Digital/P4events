dce04335ef24cc3048705d3b65993053b11ea8f2
nlloydw <nlloydw61@gmail.co.uk>
2023-06-20 14:25:27 +0100

    added seo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,6 +13,8 @@
         gtag('config', 'G-KG18K41GHV');
     </script>
 
+    @seo_pro('meta')
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
