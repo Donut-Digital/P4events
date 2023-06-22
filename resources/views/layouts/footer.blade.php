486aaefd5dd469568ea2a5335f0971cf548fbb17
nlloydw <nlloydw61@gmail.co.uk>
2023-06-22 12:28:28 +0100

    updated footer socials

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -18,56 +18,56 @@
 
                 <!-- Telephone -->
                 <div class="flex">
-                    <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
-                        <i class="text-xs fa-solid fa-phone text-nav-secondary"></i>
-                    </div>
-
-                    <a href="tel:{{ $company_details['telephone'] }}" aria-label="Company Telephone" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base"></a>
+                    <a href="tel:{{ $company_details['telephone'] }}" aria-label="Company Telephone" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base">
+                        <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
+                            <i class="text-xs fa-solid fa-phone text-nav-secondary"></i>
+                        </div>
+                    </a>
                 </div>
 
                 <!-- Email -->
                 <div class="flex">
-                    <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
-                        <i class="text-xs fa-solid fa-envelope text-nav-secondary"></i>
-                    </div>
-
-                    <a href="mailto:{{ $company_details['email'] }}" aria-label="Company Email" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base"></a>
+                    <a href="mailto:{{ $company_details['email'] }}" aria-label="Company Email" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base">
+                        <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
+                            <i class="text-xs fa-solid fa-envelope text-nav-secondary"></i>
+                        </div>
+                    </a>
                 </div>
 
                 <!-- Facebook -->
                 <div class="flex">
-                    <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
-                        <i class="text-xs fa-brands fa-facebook-f text-nav-secondary"></i>
-                    </div>
-
-                    <a href="tel:{{ $company_details['facebook'] }}" aria-label="Company Facebook" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base"></a>
+                    <a href="tel:{{ $company_details['facebook'] }}" aria-label="Company Facebook" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base">
+                        <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
+                            <i class="text-xs fa-brands fa-facebook-f text-nav-secondary"></i>
+                        </div>
+                    </a>
                 </div>
 
                 <!-- Twitter -->
                 <div class="flex">
-                    <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
-                        <i class="text-xs fa-brands fa-twitter text-nav-secondary"></i>
-                    </div>
-
-                    <a href="tel:{{ $company_details['twitter'] }}" aria-label="Company Twitter" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base"></a>
+                    <a href="tel:{{ $company_details['twitter'] }}" aria-label="Company Twitter" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base">
+                        <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
+                            <i class="text-xs fa-brands fa-twitter text-nav-secondary"></i>
+                        </div>
+                    </a>
                 </div>
 
                 <!-- Pinterest -->
                 <div class="flex">
-                    <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
-                        <i class="text-xs fa-brands fa-pinterest-p text-nav-secondary"></i>
-                    </div>
-
-                    <a href="tel:{{ $company_details['pinterest'] }}" aria-label="Company Pinterest" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base"></a>
+                    <a href="tel:{{ $company_details['pinterest'] }}" aria-label="Company Pinterest" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base">
+                        <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
+                            <i class="text-xs fa-brands fa-pinterest-p text-nav-secondary"></i>
+                        </div>
+                    </a>
                 </div>
 
                 <!-- Instagram -->
                 <div class="flex">
-                    <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
-                        <i class="text-xs fa-brands fa-instagram text-nav-secondary"></i>
-                    </div>
-
-                    <a href="tel:{{ $company_details['instagram'] }}" aria-label="Company Instagram" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base"></a>
+                    <a href="tel:{{ $company_details['instagram'] }}" aria-label="Company Instagram" class="text-sm font-light text-white transition-all hover:text-primary lg:text-base">
+                        <div class="my-auto mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-white">
+                            <i class="text-xs fa-brands fa-instagram text-nav-secondary"></i>
+                        </div>
+                    </a>
                 </div>
             </div>
 
