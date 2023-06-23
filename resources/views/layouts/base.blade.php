b34a34e439ce2c82db75d7efc3724d82f5d780ab
nlloydw <nlloydw61@gmail.co.uk>
2023-06-23 18:54:53 +0100

    remove duplicate title tag

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -30,9 +30,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
 
-    <!-- Title -->
-    <title>{{ $company_details['company_name'] }}</title>
-
 </head>
 
 <body>
