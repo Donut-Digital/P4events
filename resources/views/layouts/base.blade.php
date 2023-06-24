9396f11b87233ee8d3e910db02355d90a492fc75
nlloydw <nlloydw61@gmail.co.uk>
2023-06-24 20:42:58 +0100

    font loading deffered

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -28,7 +28,20 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+    
+    <link
+        rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&family=Lato:wght@100;300;400;700;900&display=swap"
+        as="style"
+        onload="this.onload=null;this.rel='stylesheet'"
+    />
+    <noscript>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&family=Lato:wght@100;300;400;700;900&display=swap"
+            rel="stylesheet"
+            type="text/css"
+        />
+    </noscript>
 
 </head>
 
