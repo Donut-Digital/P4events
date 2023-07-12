7553aea5806158a70142ddaab36a7be4f2f0e11e
nlloydw <nlloydw61@gmail.co.uk>
2023-07-12 10:35:55 +0100

    potential bug fix

--- resources/js/modules/Recaptcha.js
+++ resources/js/modules/Recaptcha.js
@@ -1,3 +1,5 @@
+import grecaptcha from "laravel-mix/src/Dependencies";
+
 export const generateRecaptchaToken = async function () {
   return grecaptcha.execute("6LdXbLMlAAAAAJg8pbUQIzEW5jKRWorqlQY1XiNr", {
     action: "submit",
