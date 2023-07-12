51d08464d791f6976c5ed5d4158739ce5af88d49
nlloydw <nlloydw61@gmail.co.uk>
2023-07-12 10:41:48 +0100

    bug fix broke everything - removed it

--- resources/js/modules/Recaptcha.js
+++ resources/js/modules/Recaptcha.js
@@ -1,5 +1,3 @@
-import grecaptcha from "laravel-mix/src/Dependencies";
-
 export const generateRecaptchaToken = async function () {
   return grecaptcha.execute("6LdXbLMlAAAAAJg8pbUQIzEW5jKRWorqlQY1XiNr", {
     action: "submit",
